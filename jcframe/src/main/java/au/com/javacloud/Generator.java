4f7b1bc23486e40d1a2b1aeaaac2f0eced15e7ad
Digital Spider <dev@digitalspider.com.au>
2016-06-18 07:54:35 +1000

    Organise imports

--- jcframe/src/main/java/au/com/javacloud/Generator.java
+++ jcframe/src/main/java/au/com/javacloud/Generator.java
@@ -1,22 +1,11 @@
 package au.com.javacloud;
 
-import java.io.File;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
-
-import au.com.javacloud.annotation.ExcludeDBWrite;
-import au.com.javacloud.annotation.IndexPage;
-import au.com.javacloud.model.BaseBean;
 import au.com.javacloud.util.Statics;
 import au.com.javacloud.view.ViewGenerator;
-import au.com.javacloud.view.ViewGeneratorImpl;
-import au.com.javacloud.view.ViewType;
 
 /**
  * Created by david on 26/05/16.
